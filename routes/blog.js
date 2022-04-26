ec1461108db74eb4f3c94c898a11cb382ea9feed
canozzcan <canozcanstu@gmail.com>
2022-04-26 16:00:37 +0300

    Inserting - Fetching And Displaying Entering Post

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongodb = require('mongodb');
 
 const db = require('../data/database');
+
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
@@ -8,13 +11,54 @@ router.get('/', function (req, res) {
   res.redirect('/posts');
 });
 
-router.get('/posts', function (req, res) {
-  res.render('posts-list');
+router.get('/posts', async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection('posts')
+    .find({})
+    .project({ title: 1, summary: 1, 'author.name': 1 })
+    .toArray();
+  res.render('posts-list', { posts: posts });
 });
 
 router.get('/new-post', async function (req, res) {
   const authors = await db.getDb().collection('authors').find().toArray(); // db.getDb().collection('authors') = > That's how we establish access to the author's collection in our MongoDb database
   res.render('create-post', { authors: authors });
 });
+
+router.post('/posts', async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db.getDb().collection('authors').findOne({ _id: authorId });
+
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email
+    }
+  };
+
+  const result = await db.getDb().collection('posts').insertOne(newPost);
+  console.log(result);
+  res.redirect('/posts');
+});
+
+router.get('/posts/:id', async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection('posts')
+    .findOne({ _id: new ObjectId(postId) }, { summary: 0 });  //summary: 0 => means in this code is excluded
+  
+  if (!post) {
+    return res.status(404).render('404');
+  }
+
+  res.render('post-detail', {post: post})
+})
 
 module.exports = router;
